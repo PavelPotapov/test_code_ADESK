9f6942876f263deda9b919da6ec064f393aef3ea
PavelPotapov <pavelpotapovv96@gmail.com>
2023-11-01 14:26:05 +0500

    fix-bugs, create class AccordionHelper

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,49 +1,91 @@
-export default function () {
-	const accordions = Array.from(document.querySelectorAll(".accordion__btn"))
-	accordions.forEach((acc) => {
-		acc.addEventListener("click", (e) => {
-			e.preventDefault()
-			e.currentTarget.classList.toggle("accordion__btn--active")
-			const currentAccordion = e.target.closest(".accordion")
-			const currentContent = currentAccordion.querySelector(
-				".accordion__content"
-			)
-			currentContent.classList.toggle("accordion__content--active")
-			if (currentContent.classList.contains("accordion__content--active")) {
-				currentContent.style.maxHeight = currentContent.scrollHeight + "px" // открываем контент
-			} else {
-				currentContent.style.maxHeight = 0
-			}
-		})
-	})
-
-	/* пишутся инлайновые стили для контента аккордиона, их надо удалять, чтобы не ломалась верстка  */
-	/* также можно реализовать это через css переменную */
-	function clearMaxHeight() {
-		const accordionsContents = Array.from(
-			document.querySelectorAll(".accordion__content")
-		)
-		accordionsContents.forEach((acc) => {
-			if (acc.style.removeProperty) {
-				acc.style.removeProperty("max-height")
-			} else {
-				acc.style.removeAttribute("max-height")
-			}
-		})
+class AccordionHelper {
+	constructor(node) {
+		this.accordion = node
+		this.isTriggered = false
+		this.init()
+		this.matchMedia()
 	}
 
-	/* при уходе с мобильного разрешения удаляем инлайновые стили высоты */
-	const mobileWidthMediaQuery = window.matchMedia("(max-width: 600px)")
+	//получает data-js-accordion для определения параметров отображения
+	//по сути можно развивать функциональность, в этом атрибуте будут храниться стартовые параметры аккордеона
+	//сейчас там находится информация о ширине, с которой аккордеон начинает свою работу
+	init() {
+		this.parameters = JSON.parse(this.accordion.dataset.jsAccordion)
+		if (this.parameters.hasOwnProperty("width")) {
+			this.width = this.parameters.width
+		}
 
-	function trigger(isMobileSize) {
-		if (!isMobileSize) {
-			clearMaxHeight()
+		this.btn = this.accordion.querySelector(".accordion-btn") //кнопка аккордеона
+		this.content = this.accordion.querySelector(".accordion-content") //контент
+	}
+
+	//при изменении viewport надо удалить inline стили максимальной высоты, записанные туда ранее, чтобы не ломалась верстка
+	clearMaxHeight() {
+		if (this.content) {
+			if (this.content.style?.removeProperty) {
+				this.content.style.removeProperty("max-height")
+			} else {
+				this.content.style.removeAttribute("max-height")
+			}
 		}
 	}
 
-	trigger(mobileWidthMediaQuery.matches)
+	hide() {
+		this.btn.classList.remove("accordion-btn--hide")
+		this.content.classList.remove("accordion-content--active")
+	}
 
-	mobileWidthMediaQuery.addEventListener("change", function (event) {
-		trigger(event.matches)
+	show() {
+		this.btn.classList.add("accordion-btn--hide")
+		this.btn.classList.remove("accordion-btn--active")
+		this.content.classList.add("accordion-content--active")
+	}
+
+	//срабатывающий триггер. когда сузим до нужной ширины или наоборот расширим больше, чем нужно, срабатывает эта функция
+	trigger(isTriggerSize) {
+		if (this.btn && this.content) {
+			if (isTriggerSize) {
+				this.hide()
+				this.isTriggered = true
+			} else {
+				this.clearMaxHeight()
+				this.show()
+				this.isTriggered = false
+			}
+		}
+	}
+
+	//функция запроса mathcmedia параметров и подписка на прослушивание этого события
+	matchMedia() {
+		if (this.width) {
+			this.widthMediaQuery = window.matchMedia(`(max-width: ${this.width}px)`)
+			this.trigger(this.widthMediaQuery.matches)
+			this.widthMediaQuery.addEventListener("change", (event) => {
+				this.trigger(event.matches)
+			})
+		}
+	}
+
+	handleСlick() {
+		this.accordion.addEventListener("click", (e) => {
+			if (this.isTriggered && this.content && this.btn) {
+				e.preventDefault()
+				this.btn.classList.toggle("accordion-btn--active")
+				this.content.classList.toggle("accordion-content--active")
+				if (this.content.classList.contains("accordion-content--active")) {
+					this.content.style.maxHeight = this.content.scrollHeight + "px"
+				} else {
+					this.content.style.maxHeight = 0
+				}
+			}
+		})
+	}
+}
+
+export function initAccordions() {
+	const accordions = Array.from(document.querySelectorAll(".accordion"))
+	accordions.forEach((accordion) => {
+		const acc = new AccordionHelper(accordion)
+		acc.handleСlick()
 	})
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@ import "../styles/scss/main.scss"
 
 import initBurger from "./burger"
 import initMatch from "./crossButtons"
-import initAccordion from "./accordion"
+import { initAccordions } from "./accordion"
 
 initBurger()
 initMatch()
-initAccordion()
+initAccordions()
